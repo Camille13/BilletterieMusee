52eade9e5f400269d78fd7a0bb16fce028065a8d
Camille13 <camille.palpacuer@gmail.com>
2016-10-04 22:17:46 +0200

    Dependancy var supperflus

--- src/Musee/BilletterieBundle/DependencyInjection/Configuration.php
+++ src/Musee/BilletterieBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('musee_billetterie');
+        $treeBuilder->root('musee_billetterie');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
